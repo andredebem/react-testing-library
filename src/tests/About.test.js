57dd14b04afd84168bc3127caefab409652ff61c
André De Bem <andrebemferraz@gmail.com>
2021-05-26 12:14:30 -0300

    Request II

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,35 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../renderWithRouter';
+import About from '../components/About';
+
+test('A página contém as informações sobre a Pokédex', () => {
+  const { getByText } = renderWithRouter(<About />);
+
+  const info = getByText(/a digital encyclopedia containing all Pokémons/i);
+  expect(info).toBeInTheDocument();
+});
+
+test('A página contém um heading h2 com o texto "About Pokédex" ', () => {
+  const { getByRole } = renderWithRouter(<About />);
+
+  const title = getByRole('heading', {
+    name: 'About Pokédex',
+    level: 2,
+  });
+  expect(title).toBeInTheDocument();
+});
+
+test('A página contém dois parágrafos com texto sobre a Pokédex', () => {
+  const { getAllByText } = renderWithRouter(<About />);
+
+  const p = getAllByText(/Pokémons/i);
+  expect(p.length).toBe(2);
+});
+
+test('A página contém a imagem correta de uma Pokédex', () => {
+  const { getByAltText } = renderWithRouter(<About />);
+
+  const image = getByAltText('Pokédex');
+  const source = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+  expect(image.src).toBe(source);
+});
